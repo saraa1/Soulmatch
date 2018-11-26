a3313d2da6a4bcaf03ffde36590389fb3307e352
afifa saleen <afifasaleem77@gmail.com>
2018-11-26 21:01:10 +0500

    DB
    Db and all files and links corrected

---
+++ SoulMatch/SoulMatch/Controllers/BrokerDbsController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SoulMatch.Models;
+
+
+namespace SoulMatch.Controllers
+{
+    public class BrokerDbsController : Controller
+    {
+        private SoulMatchEntities db = new SoulMatchEntities();
+
+        // GET: BrokerDbs
+        public ActionResult Index()
+        {
+            return View(db.BrokerDbs.ToList());
+        }
+
+        // GET: BrokerDbs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BrokerDb brokerDb = db.BrokerDbs.Find(id);
+            if (brokerDb == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brokerDb);
+        }
+
+        // GET: BrokerDbs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BrokerDbs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Code,Name,Email,City,CNIC,ContactNo,Adress")] BrokerDb brokerDb)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BrokerDbs.Add(brokerDb);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(brokerDb);
+        }
+
+        // GET: BrokerDbs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BrokerDb brokerDb = db.BrokerDbs.Find(id);
+            if (brokerDb == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brokerDb);
+        }
+
+        // POST: BrokerDbs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Code,Name,Email,City,CNIC,ContactNo,Adress")] BrokerDb brokerDb)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(brokerDb).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(brokerDb);
+        }
+
+        // GET: BrokerDbs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BrokerDb brokerDb = db.BrokerDbs.Find(id);
+            if (brokerDb == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brokerDb);
+        }
+
+        // POST: BrokerDbs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BrokerDb brokerDb = db.BrokerDbs.Find(id);
+            db.BrokerDbs.Remove(brokerDb);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+        //FAFA //
+
+        // GET: broker
+        [HttpGet]
+        public ActionResult Login()
+        {
+            BrokerDb Brokermodel = new BrokerDb();
+            return View(Brokermodel);
+
+        }
+
+        [HttpPost]
+        public ActionResult Login(BrokerDb brokermodel)
+        {
+
+            using (SoulMatchEntities db = new SoulMatchEntities())
+            {
+
+                if (db.BrokerDbs.Any(x => x.Id == brokermodel.Id && x.Code == brokermodel.Code))
+                {
+
+                    ViewBag.SuccessMessage = "Login Successful";
+                    Session["BrokerID"] = brokermodel.Id;
+                    return RedirectToAction("pro", "User");
+                }
+                ViewBag.LoginErrorMessage = "Wrong Email and password";
+                return View("Login", brokermodel);
+            }
+
+        }
+        public ActionResult Userprofile()
+        {
+            UserDb usermodel = new UserDb();
+            return View(usermodel);
+        }
+
+
+        public ActionResult Profile()
+        {
+            SoulMatchEntities db = new SoulMatchEntities();
+            var result = (from c in db.UserDbs
+                          select new Gridtables
+                          {
+                              UserID = c.UserID,
+                              Image = c.Image,
+                              Username = c.Username,
+                              Password = c.Password,
+                              Age = c.Age,
+                              CNIC = c.CNIC,
+                              Address = c.Address,
+                              Contact_no = c.Contact_no,
+                              Email = c.Email,
+                              Salary = c.Salary,
+                              Gender = c.Gender,
+                              Religion = c.Religion,
+                              Caste = c.Caste,
+                              Profession = c.Profession,
+                              Account_no = c.Account_no
+
+                          }).ToList();
+            return View(result);
+
+        }
+
+        //FAFA  //
+    }
+}
---
+++ SoulMatch/SoulMatch/Controllers/UserDbsController.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SoulMatch.Models;
+using System.Linq.Expressions;
+using System.IO;
+
+namespace SoulMatch.Controllers
+{
+    public class UserDbsController : Controller
+    {
+        private SoulMatchEntities db = new SoulMatchEntities();
+
+        // GET: UserDbs
+        public ActionResult Index()
+        {
+            return View(db.UserDbs.ToList());
+        }
+
+        // GET: UserDbs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserDb userDb = db.UserDbs.Find(id);
+            if (userDb == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userDb);
+        }
+
+        // GET: UserDbs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserDbs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserId,Fullname,Username,Password,Gender,Age,Cast,Religion,City,Adress,Contact_No,CNIC,Profession,Salary")] UserDb userDb)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserDbs.Add(userDb);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userDb);
+        }
+
+        // GET: UserDbs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserDb userDb = db.UserDbs.Find(id);
+            if (userDb == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userDb);
+        }
+
+        // POST: UserDbs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserId,Fullname,Username,Password,Gender,Age,Cast,Religion,City,Adress,Contact_No,CNIC,Profession,Salary")] UserDb userDb)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userDb).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userDb);
+        }
+
+        // GET: UserDbs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserDb userDb = db.UserDbs.Find(id);
+            if (userDb == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userDb);
+        }
+
+        // POST: UserDbs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserDb userDb = db.UserDbs.Find(id);
+            db.UserDbs.Remove(userDb);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+
+        //SARA //
+
+        [HttpGet]
+        public ActionResult Register()
+
+        {
+            UserDb usermodel = new UserDb();
+            return View(usermodel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(UserDb usermodel)
+        {
+            string filename = Path.GetFileNameWithoutExtension(usermodel.Imagefile.FileName);
+            string extension = Path.GetExtension(usermodel.Imagefile.FileName);
+            filename = filename + DateTime.Now.ToString("yymmssfff") + extension;
+            usermodel.Image = "~/Image/" + filename;
+            filename = Path.Combine(Server.MapPath("~/Image/"), filename);
+            usermodel.Imagefile.SaveAs(filename);
+
+            using (SoulMatchEntities dbModel = new SoulMatchEntities())
+
+            {
+
+                if (dbModel.UserDbs.Any(x => x.Username == usermodel.Username))
+                {
+                    ViewBag.DuplicateMessage = "Username already exists";
+                    return View("Register", usermodel);
+                }
+                dbModel.UserDbs.Add(usermodel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registeration successful";
+            return View("Register", new UserDb());
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            UserDb usermodel = new UserDb();
+            return View(usermodel);
+
+        }
+        [HttpPost]
+        public ActionResult Login(UserDb usermodel)
+        {
+
+            using (SoulMatchEntities db = new SoulMatchEntities())
+            {
+
+                if (db.UserDbs.Any(x => x.Email == usermodel.Email && x.Password == usermodel.Password))
+                {
+
+                    ViewBag.SuccessMessage = "Login Successful";
+                    Session["userID"] = usermodel.UserID;
+                    return RedirectToAction("UserProfile", "User");
+                }
+                ViewBag.LoginErrorMessage = "Wrong Email and password";
+                return View("Login", usermodel);
+            }
+
+        }
+        public ActionResult Userprofile()
+        {
+            UserDb usermodel = new UserDb();
+            return View(usermodel);
+        }
+        [HttpGet]
+        public ActionResult Searchuser(string searching)
+        {
+            var user = from s in db.RegisteredUserDbs
+                       select s;
+            if (!String.IsNullOrEmpty(searching))
+            {
+
+                user = user.Where(s => s.Religion.Contains(searching) || s.Profession.Contains(searching) || s.Cast.Contains(searching) || searching == null);
+            }
+
+
+            return View(user.ToList());
+
+        }
+
+        //SARA //
+    }
+}
